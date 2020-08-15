1e02e1f9a985bee5f26f7a0eace22ef6cb5ee80e
Darko Draskovic <darko.draskovic@gmail.com>
2020-08-15 13:01:04 +0200

    Add CMake util fn to add targets
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

---
+++ CMake/Functions.cmake
@@ -0,0 +1,6 @@
+function(add_target TARGET)
+  file(GLOB APP_C "${SAMPLES}/${TARGET}/*.cpp")
+  add_executable(${TARGET} ${APP_C})
+  target_include_directories(${TARGET} PUBLIC "${SAMPLES}/${TARGET}/")
+  target_link_libraries(${TARGET} GL GLU glfw dl engine shapeFactory)
+endfunction()
